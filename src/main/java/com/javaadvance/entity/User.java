1c5a909e2248cfd0df2b41eb5767f089840ab91c
Pazl2 <pavel.yasinsky2006@gmail.com>
2026-05-14 22:45:45 +0300

    add env variables

--- src/main/java/com/javaadvance/entity/User.java
+++ src/main/java/com/javaadvance/entity/User.java
@@ -1,52 +1,52 @@
-package com.javaadvance.entity;
+    package com.javaadvance.entity;
 
 
-import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+    import jakarta.persistence.*;
+    import org.springframework.data.annotation.CreatedDate;
+    import org.springframework.data.annotation.LastModifiedDate;
+    import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+    import java.time.LocalDate;
+    import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "users", indexes = {
-        @Index(name = "idx_users_email", columnList = "email"),
-        @Index(name = "idx_users_active", columnList = "active")
-})
-@EntityListeners(AuditingEntityListener.class)
-public class User {
+    @Entity
+    @Table(name = "users", indexes = {
+            @Index(name = "idx_users_email", columnList = "email"),
+            @Index(name = "idx_users_active", columnList = "active")
+    })
+    @EntityListeners(AuditingEntityListener.class)
+    public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
-    @Column(name = "name")
-    private String name;
+        @Column(name = "name")
+        private String name;
 
-    @Column(name = "surname")
-    private String surname;
+        @Column(name = "surname")
+        private String surname;
 
-    @Column(name = "birth_date")
-    private LocalDate birthDate;
+        @Column(name = "birth_date")
+        private LocalDate birthDate;
 
-    @Column(name = "email",
-            unique = true,
-            nullable = false)
-    private String email;
+        @Column(name = "email",
+                unique = true,
+                nullable = false)
+        private String email;
 
-    @Column(name = "active",
-            nullable = false)
-    private boolean active;
+        @Column(name = "active",
+                nullable = false)
+        private boolean active;
 
-    @CreatedDate
-    @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt;
+        @CreatedDate
+        @Column(name = "created_at", updatable = false)
+        private LocalDateTime createdAt;
 
-    @LastModifiedDate
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
+        @LastModifiedDate
+        @Column(name = "updated_at")
+        private LocalDateTime updatedAt;
 
 
 
-}
+    }
