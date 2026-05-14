5c0ca4d7775a7f2b32fbbd57b7e835d5146d275e
Pazl2 <pavel.yasinsky2006@gmail.com>
2026-05-14 23:00:49 +0300

    add lombok, entities have getters setters and noargs constructor and fixed imports (no wildcard imports)

--- src/main/java/com/javaadvance/entity/PaymentCard.java
+++ src/main/java/com/javaadvance/entity/PaymentCard.java
@@ -1,6 +1,20 @@
 package com.javaadvance.entity;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -14,6 +28,9 @@ import java.time.LocalDateTime;
         @Index(name = "idx_payment_cards_active", columnList = "active"),
         @Index(name = "idx_payment_cards_number", columnList = "number")
 })
+@Getter
+@Setter
+@NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 public class PaymentCard {
 
@@ -44,5 +61,6 @@ public class PaymentCard {
     @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
 
 }
--- src/main/java/com/javaadvance/entity/User.java
+++ src/main/java/com/javaadvance/entity/User.java
@@ -1,52 +1,63 @@
-    package com.javaadvance.entity;
+package com.javaadvance.entity;
 
 
-    import jakarta.persistence.*;
-    import org.springframework.data.annotation.CreatedDate;
-    import org.springframework.data.annotation.LastModifiedDate;
-    import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-    import java.time.LocalDate;
-    import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-    @Entity
-    @Table(name = "users", indexes = {
-            @Index(name = "idx_users_email", columnList = "email"),
-            @Index(name = "idx_users_active", columnList = "active")
-    })
-    @EntityListeners(AuditingEntityListener.class)
-    public class User {
+@Entity
+@Table(name = "users", indexes = {
+        @Index(name = "idx_users_email", columnList = "email"),
+        @Index(name = "idx_users_active", columnList = "active")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class User {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        @Column(name = "name")
-        private String name;
+    @Column(name = "name")
+    private String name;
 
-        @Column(name = "surname")
-        private String surname;
+    @Column(name = "surname")
+    private String surname;
 
-        @Column(name = "birth_date")
-        private LocalDate birthDate;
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
 
-        @Column(name = "email",
-                unique = true,
-                nullable = false)
-        private String email;
+    @Column(name = "email",
+            unique = true,
+            nullable = false)
+    private String email;
 
-        @Column(name = "active",
-                nullable = false)
-        private boolean active;
+    @Column(name = "active",
+            nullable = false)
+    private boolean active;
 
-        @CreatedDate
-        @Column(name = "created_at", updatable = false)
-        private LocalDateTime createdAt;
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
 
-        @LastModifiedDate
-        @Column(name = "updated_at")
-        private LocalDateTime updatedAt;
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
-
-
-    }
+}
