a46817443dd326c1c5d641013cf0b07276610767
Pazl2 <pavel.yasinsky2006@gmail.com>
2026-05-31 18:18:54 +0300

    add roles to controllers

--- src/main/java/com/innowise/userservice/controller/PaymentCardController.java
+++ src/main/java/com/innowise/userservice/controller/PaymentCardController.java
@@ -7,15 +7,8 @@ import com.innowise.userservice.service.PaymentCardService;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cards")
@@ -23,11 +16,12 @@ public class PaymentCardController {
 
     private final PaymentCardService paymentCardService;
 
-    public PaymentCardController(PaymentCardService paymentCardService){
+    public PaymentCardController(PaymentCardService paymentCardService) {
         this.paymentCardService = paymentCardService;
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Page<PaymentCardResponse>> getCards(
             @RequestParam(required = false) String firstName,
             @RequestParam(required = false) String surname,
@@ -39,30 +33,31 @@ public class PaymentCardController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<PaymentCardResponse> getCardById(@PathVariable Long id){
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<PaymentCardResponse> getCardById(@PathVariable Long id) {
         return ResponseEntity.ok().body(paymentCardService.getCardById(id));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<PaymentCardResponse> updateCard(
             @PathVariable Long id,
-            @RequestBody @Valid PaymentCardUpdateRequest request){
-        PaymentCardResponse response = paymentCardService.updatePaymentCard(id, request);
-        return ResponseEntity.ok().body(response);
+            @RequestBody @Valid PaymentCardUpdateRequest request) {
+        return ResponseEntity.ok().body(paymentCardService.updatePaymentCard(id, request));
     }
 
     @PatchMapping("/{id}/active")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<PaymentCardResponse> updateActive(
             @PathVariable Long id,
-            @RequestBody @Valid ActiveStatusRequest active){
-        return ResponseEntity.ok().body(
-                paymentCardService.updateActivity(id, active.isActive()));
+            @RequestBody @Valid ActiveStatusRequest active) {
+        return ResponseEntity.ok().body(paymentCardService.updateActivity(id, active.isActive()));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCard(@PathVariable Long id){
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> deleteCard(@PathVariable Long id) {
         paymentCardService.deleteCard(id);
         return ResponseEntity.noContent().build();
     }
-
 }
