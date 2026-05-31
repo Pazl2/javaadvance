afbafd9e6b2453bca81321c72a51cf40be88198d
Pazl2 <pavel.yasinsky2006@gmail.com>
2026-05-31 19:20:55 +0300

    fix security in tests 4

--- src/test/java/com/innowise/userservice/integration/config/TestSecurityConfig.java
+++ src/test/java/com/innowise/userservice/integration/config/TestSecurityConfig.java
@@ -2,16 +2,16 @@ package com.innowise.userservice.integration.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @TestConfiguration
+@EnableMethodSecurity(prePostEnabled = false)
 public class TestSecurityConfig {
 
     @Bean
-    @Primary
     public SecurityFilterChain testSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
